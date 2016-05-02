35a92b179815e6669fe2a78f91c19722e6705fe2
Christofer Peterson <cpeterson@healthgrades.com>
2016-05-02 15:03:50 -0600

    Playing with props

--- components.js
+++ components.js
@@ -1,22 +1,48 @@
-class StoryBox extends React.Component {
-  render() {
-    const now = new Date();
-    const topicsList = ['HTML', 'JavaScript', 'React'];
+(function() {
+  'use strict';
 
-    return(
-      <div>
-        <h3>Stories App</h3>
-        <p className="lead">
-          Current time: {now.toTimeString()}
-        </p>
-        <ul>
-          {topicsList.map(topic => <li>{topic}</li> )}
-        </ul>
-      </div>
-    );
+  class StoryBox extends React.Component {
+    getInitialState() {
+      return {
+        items: []
+      };
+    }
+
+    addItem() {
+
+    }
+
+    render() {
+      var name;
+      var time;
+      var items;
+
+      var { name, time, items } = this.props;
+
+      return(
+        <div>
+          <h3>{name}</h3>
+          <p className="lead">
+            Current time: {time}
+          </p>
+          <form>
+            <input type="text" /><button type="submit">Add Item</button>
+          </form>
+          <ul>
+            {items.map(item => <li>{item}</li> )}
+          </ul>
+        </div>
+      );
+    }
   }
-}
 
-ReactDOM.render(
-  <StoryBox />, document.getElementById('story-app')
-);
+  var props = {
+    name:"Stories App",
+    time:new Date().toTimeString(),
+    items: ['HTML', 'JavaScript', 'React']
+  };
+
+  ReactDOM.render(
+    <StoryBox {...props} />, document.getElementById('story-app')
+  );
+})();
